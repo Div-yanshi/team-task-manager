71d1fe841be41573bf350e1b44c858f8b33c2a16
Div-yanshi <2k22.csds.ng2.2212527@gmail.com>
2026-05-01 04:28:17 +0530

    Update frontend API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://team-task-manager-production-b0e0.up.railway.app/api"
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
